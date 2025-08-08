94c4f80dff417eb3a75d8110379beae5166ee16a
justprog973 <edge973@hotmail.fr>
2025-08-08 11:50:37 -0300

    Fix: column css footer generate

--- themes/heros/template-parts/footer/widgets.php
+++ themes/heros/template-parts/footer/widgets.php
@@ -2,7 +2,7 @@
 $footer_layout = sanitize_text_field(get_theme_mod('_themename_footer_layout', '3,3,3,3'));
 $footer_layout = preg_replace('/\s+/','', $footer_layout);
 $columns= explode(',', $footer_layout);
-$addClassCol = preg_filter('/^/', 'col-span-full  lg:col-span-', $columns);
+$addClassCol = preg_filter('/^/', 'col-span-full  md:col-span-', $columns);
 $widgets_active = false;
 foreach ($columns as $i => $column){
     if(is_active_sidebar('footer-sidebar-'.($i + 1))) {
@@ -13,7 +13,7 @@ foreach ($columns as $i => $column){
 <?php if($widgets_active) : ?>
     <div class="bg-slate-50">
         <div class="max-w-7xl w-full mx-auto py-6 px-4">
-            <div class="grid lg:grid-cols-12 grid-flow-col auto-cols-fr gap-10 md:grid-cols-1 wp-content">
+            <div class="grid grid-cols-1 md:grid-cols-12 grid-flow-col auto-cols-fr gap-5  wp-content">
                 <?php foreach ($columns as $i => $column):?>
                     <div class="<?php echo $addClassCol[$i] ?>">
                         <?php if(is_active_sidebar('footer-sidebar-'.($i + 1))): ?>
