c86516fdd74556850a590839a38d2f10b1d06fd7
justprog973 <edge973@hotmail.fr>
2025-08-09 16:06:50 -0300

    Fix: responsive design single page

--- themes/heros/template-parts/single/content.php
+++ themes/heros/template-parts/single/content.php
@@ -1,22 +1,22 @@
 <article <?php post_class("jp-article bg-white border-t border-gray-100") ?>>
     <?php if (get_the_post_thumbnail() !== ""): ?>
-        <header style="background-image: url(<?php the_post_thumbnail_url('full') ?>)" class="jp-article__header before:bg-black/75 before:h-full before:w-full before:absolute relative card-thumbnail w-full h-[350px] bg-no-repeat bg-center bg-cover">
+        <header style="background-image: url(<?php the_post_thumbnail_url('full') ?>)" class="jp-article__header before:bg-black/75 before:h-full before:w-full before:absolute relative card-thumbnail w-full h-[200px] md:h-[250px] bg-no-repeat bg-center bg-cover">
     <?php else: ?>
             <header class="jp-article__header w-full bg-gray-800 py-10">
    <?php endif; ?>
             <div class="header-single-container relative z-10 jp-container h-full flex justify-start items-center">
                 <div class="flex flex-col">
                     <?php the_wp_permalink_custom(); ?>
-                    <p class="text-slate-300 text-sm italic mb-2"><?php _themename_post_meta(); ?></p>
+                    <p class="text-slate-300 text-sm md:text-dase italic mb-2"><?php _themename_post_meta(); ?></p>
                     <h1 class="card-title">
-                        <a class="card-title__link text-5xl text-white" href="<?php the_permalink(); ?>" title="<?php the_title_attribute() ?>">
+                        <a class="card-title__link text-2xl md:text-5xl text-white" href="<?php the_permalink(); ?>" title="<?php the_title_attribute() ?>">
                             <?php the_title(); ?>
                         </a>
                     </h1>
                 </div>
             </div>
         </header>
-    <div class="jp-article__content py-5">
+    <div class="jp-article__content">
         <div class="jp-container text-base">
             <div class="mb-5 wp-content gap-10 grid grid-flow-row lg:grid-cols-12 sm:grid-cols-1">
                 <div class="max-w-7xl w-full mx-auto wp-content <?php if (is_active_sidebar('primary-sidebar')): echo "lg:col-span-9 md:col-span-12"; else: echo "col-span-12"; endif; ?>">
